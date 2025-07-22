0167fd288226054eac9e551adee2df491131c844
mustafacaglarr <mustafa.caglar147@gmail.com>
2025-07-23 01:03:31 +0300

    Color constants

---
+++ lib/shared/widgets/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color.fromARGB(255, 241, 241, 241);   // Arka plan
+const Color kHeaderBlue = Color(0xFF1976FF);        // Üst alan (ör: #1976FF)
+const Color kTextDark = Color.fromARGB(255, 0, 0, 0);          // Koyu metin
+const Color kTextMedium = Color(0xFF666666);        // Orta gri
+const Color kTextLight = Color(0xFFFFFFFF);         // Beyaz
+const Color kStatDownloadBg = Color(0xFFE4FBF6);    // Download kart arka planı
+const Color kStatDownload = Color(0xFF4ED8A9);      // Download ikon rengi
+const Color kStatUploadBg = Color(0xFFFEECEC);      // Upload kart arka planı
+const Color kStatUpload = Color(0xFFF16060);        // Upload ikon rengi
