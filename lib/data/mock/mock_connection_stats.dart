451fd57bd9d97fad9b15fe6d520976bcfd58a33d
mustafacaglarr <mustafa.caglar147@gmail.com>
2025-07-23 01:04:00 +0300

    Ekstra veri eklendi

--- lib/data/mock/mock_connection_stats.dart
+++ lib/data/mock/mock_connection_stats.dart
@@ -1,9 +1,25 @@
 import '../../core/models/connection_stats.dart';
 import 'mock_countries.dart';
 
-final mockConnectionStats = ConnectionStats(
-  downloadSpeed: 527,
-  uploadSpeed: 49,
-  connectedTime: Duration(hours: 2, minutes: 41, seconds: 52),
-  connectedCountry: mockCountries[1], // Netherlands
-);
+
+/// Ülkeye göre farklı hız ve başlangıç zamanı tutan liste
+final mockConnectionStatsList = [
+  ConnectionStats(
+    downloadSpeed: 220,
+    uploadSpeed: 30,
+    connectedTime: Duration.zero,
+    connectedCountry: mockCountries[0], // Italy
+  ),
+  ConnectionStats(
+    downloadSpeed: 527,
+    uploadSpeed: 49,
+    connectedTime: Duration.zero,
+    connectedCountry: mockCountries[1], // Netherlands
+  ),
+  ConnectionStats(
+    downloadSpeed: 430,
+    uploadSpeed: 60,
+    connectedTime: Duration.zero,
+    connectedCountry: mockCountries[2], // Germany
+  ),
+];
--- lib/data/mock/mock_countries.dart
+++ lib/data/mock/mock_countries.dart
@@ -1,13 +1,7 @@
 import '../../core/models/country.dart';
 
 final mockCountries = [
-  Country(
-    name: 'Italy',
-    flag: 'assets/flags/italy.png',
-    locationCount: 4,
-    strength: 70,
-  ),
-  Country(
+   Country(
     name: 'Netherlands',
     flag: 'assets/flags/netherlands.png',
     city: 'Amsterdam',
@@ -15,9 +9,70 @@ final mockCountries = [
     strength: 85,
   ),
   Country(
+    name: 'Italy',
+    flag: 'assets/flags/italy.png',
+    locationCount: 4,
+    strength: 70,
+  ),
+  Country(
     name: 'Germany',
     flag: 'assets/flags/germany.png',
     locationCount: 10,
     strength: 90,
   ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+   Country(
+    name: 'Germany',
+    flag: 'assets/flags/germany.png',
+    locationCount: 10,
+    strength: 90,
+  ),
+
 ];
